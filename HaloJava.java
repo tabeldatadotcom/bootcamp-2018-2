77e9de61a7d0e8473d03750ea6e691bfc88b290c
Dimas Maryanto <engineer.dimmaryanto93@outlook.com>
2018-04-16 14:16:24 +0700

    tambah koding

--- HaloJava.java
+++ HaloJava.java
@@ -2,5 +2,6 @@ class HaloJava{
     public static void main(String[] args){
         System.out.println("Ini file baru");
         System.out.println("ini belum di save");
+        System.out.println("ini perubahan dari server");
     }
 }
